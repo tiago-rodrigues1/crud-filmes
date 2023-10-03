1e1f0c69f7289f0980e51d54af9bae8f3fa9a031
Tiago Rodrigues <tiagords127@gmail.com>
2023-10-03 10:17:27 -0300

    Update Conexao.java

--- src/br/edu/ifrn/sc/pds/moviecrud/persistencia/Conexao.java
+++ src/br/edu/ifrn/sc/pds/moviecrud/persistencia/Conexao.java
@@ -8,7 +8,7 @@ public class Conexao {
 
     private static final String URL = "jdbc:mysql://localhost:3306/filmes_db?useTimezone=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String SENHA = "darwin1859";
+    private static final String SENHA = "root";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static Connection connection;
 
